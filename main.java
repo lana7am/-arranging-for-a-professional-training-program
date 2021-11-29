5a02353e5d2a6c16eb719ee06d16ff263241898c
lana7am <85216778+lana7am@users.noreply.github.com>
2021-11-29 06:44:24 +0300

    Update main.java

--- main.java
+++ main.java
@@ -6,12 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Lana
